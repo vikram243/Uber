f52ddf2210409196cee9c24ae1da560c94ca2ccf
VIKRAM MISHRA <vikrammishra1042@gmail.com>
2025-04-12 15:43:12 +0530

    Debugging tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-green-500 h-screen w-screen flex justify-center items-center">
+      <h1 className="text-5xl text-white font-bold">Uber Eats</h1>
+    </div>
+  )
+}
+
+export default App
