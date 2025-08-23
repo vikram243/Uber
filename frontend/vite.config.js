a0466bc88ad921ea4ef2172dd52f975f341a2787
Vikram Mishra <vikrammishra1042@gmail.com>
2025-08-23 19:06:01 +0530

    Solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(),tailwindcss()],
-  publicDir: 'public'
+  plugins: [react(),tailwindcss()]
 })
