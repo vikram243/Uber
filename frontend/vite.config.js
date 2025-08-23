a80686111d61dd314493352d105e6a35ad5b9422
Vikram Mishra <vikrammishra1042@gmail.com>
2025-08-23 18:04:14 +0530

    Fix SPA routing for Vite on Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import VitePluginRewriteAll from 'vite-plugin-rewrite-all'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePluginRewriteAll()],
+  build: {
+    outDir: 'dist'
+  },
 })
